f1a9b488e2f93e516cfa988f02b1485dcbfb7e5f
Shun Cheung <shunkongcheung@gmail.com>
2020-10-23 18:40:04 -0400

    feat(_app): add bootstrap css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps /*, AppContext */ } from 'next/app'
 import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }:AppProps) {
   return (
