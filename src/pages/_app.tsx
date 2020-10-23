fdbdf50264cb7cf5154e35f677f24795e5ba4d23
Shun Cheung <shunkongcheung@gmail.com>
2020-10-23 19:40:58 -0400

    feat(_app): firebase

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,39 @@
 import type { AppProps /*, AppContext */ } from "next/app";
 import Head from "next/head";
-import "bootstrap/dist/css/bootstrap.min.css";
+import firebase from "firebase/app";
+import { FirebaseAuthProvider } from "@react-firebase/auth";
+import "firebase/auth";
 
 import { UserContext, useUserProvider } from "../contexts/userContext";
 import Layout from "../containers/Layout";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const config = {
+  apiKey: "AIzaSyC_GSZ5S9C_6NH0mOOW7TJMJjhrUW3-h_g",
+  authDomain: "running-bubble-app.firebaseapp.com",
+  databaseURL: "https://running-bubble-app.firebaseio.com",
+  projectId: "running-bubble-app",
+  storageBucket: "running-bubble-app.appspot.com",
+  messagingSenderId: "110065026552",
+  appId: "1:110065026552:web:452714be778015d419c20d",
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
   const userContext = useUserProvider();
   return (
-    <UserContext.Provider value={userContext}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <FirebaseAuthProvider firebase={firebase} {...config}>
+      <UserContext.Provider value={userContext}>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </UserContext.Provider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </UserContext.Provider>
+    </FirebaseAuthProvider>
   );
 }
 
