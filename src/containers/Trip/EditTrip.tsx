418fa429bc6532c2787986547b8a9385ad1d2adb
kavindu <narathotakyl@gmail.com>
2020-10-24 17:33:52 -0400

    qm:kavindu:front-end:create trip modal ui updating 1

--- src/containers/Trip/EditTrip.tsx
+++ src/containers/Trip/EditTrip.tsx
@@ -1,22 +1,14 @@
-import React, { memo } from "react";
+import React, {memo} from "react";
 import axios from "axios";
 import firebase from "firebase";
-import {
-  Button,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-} from "reactstrap";
+import {Button, Form, FormGroup, Input, Label, Media, Modal, ModalBody, ModalFooter, ModalHeader,} from "reactstrap";
 
-import { InviteRequest, Trip } from "../../types";
+import {InviteRequest, Trip} from "../../types";
 import ParticipantsField from "./ParticipantsField";
 
 import classNames from "./EditTrip.module.css";
+
+import {IoIosRemoveCircleOutline} from "react-icons/io";
 
 interface EditTripModalProps {
   handleClose: (refresh?: boolean) => any;
@@ -24,9 +16,9 @@ interface EditTripModalProps {
 }
 
 const EditTripModal: React.FC<EditTripModalProps> = ({
-  handleClose,
-  isOpen,
-}) => {
+                                                       handleClose,
+                                                       isOpen,
+                                                     }) => {
   const [trip, setTrip] = React.useState<Trip>({
     name: "",
     rounds: [],
@@ -41,90 +33,90 @@ const EditTripModal: React.FC<EditTripModalProps> = ({
 
   const makeGetRequest = async (address) => {
     return axios.get(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.googleAPIKey}`
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.googleAPIKey}`
     );
   };
 
   const handleSubmit = React.useCallback(
-    async (e) => {
-      if (e) e.preventDefault();
-      const db = firebase.firestore();
+      async (e) => {
+        if (e) e.preventDefault();
+        const db = firebase.firestore();
 
-      const listAddresses = [
-        trip.startingPoint,
-        ...trip.stopPoints,
-        trip.endingPoint,
-      ];
+        const listAddresses = [
+          trip.startingPoint,
+          ...trip.stopPoints,
+          trip.endingPoint,
+        ];
 
-      const requestList = listAddresses.map((address) =>
-        makeGetRequest(address)
-      );
+        const requestList = listAddresses.map((address) =>
+            makeGetRequest(address)
+        );
 
-      const responses = await Promise.all(requestList);
-      const coords = responses.map((res) => {
-        if (
-          res.data &&
-          res.data.results[0].geometry &&
-          res.data.results[0].geometry.location
-        ) {
-          return res.data.results[0].geometry.location;
+        const responses = await Promise.all(requestList);
+        const coords = responses.map((res) => {
+          if (
+              res.data &&
+              res.data.results[0].geometry &&
+              res.data.results[0].geometry.location
+          ) {
+            return res.data.results[0].geometry.location;
+          }
+        });
+
+        try {
+          const tripRef = await db
+              .collection("trips")
+              .add({...trip, participants: [], coordinates: coords});
+          const tripUid = tripRef.id;
+
+          await Promise.all([
+            ...trip.participants.map(async (invitedUserUid: string) => {
+              const request: InviteRequest = {
+                status: "pending",
+                tripUid,
+                invitedUserUid,
+                createdAt: new Date(),
+                createdByUid: trip.createdByUid,
+              };
+              return db.collection("requests").add(request);
+            }),
+          ]);
+
+          handleClose(true);
+        } catch (err) {
+          console.error(err);
         }
-      });
-
-      try {
-        const tripRef = await db
-          .collection("trips")
-          .add({ ...trip, participants: [], coordinates: coords });
-        const tripUid = tripRef.id;
-
-        await Promise.all([
-          ...trip.participants.map(async (invitedUserUid: string) => {
-            const request: InviteRequest = {
-              status: "pending",
-              tripUid,
-              invitedUserUid,
-              createdAt: new Date(),
-              createdByUid: trip.createdByUid,
-            };
-            return db.collection("requests").add(request);
-          }),
-        ]);
-
-        handleClose(true);
-      } catch (err) {
-        console.error(err);
-      }
-    },
-    [trip]
+      },
+      [trip]
   );
 
   const handleChange = React.useCallback(
-    ({ target: { value, name, checked } }) => {
-      if (name === "participants") {
-        setTrip((o) => {
-          let { participants } = o;
-          if (checked) participants.push(value);
-          else participants = participants.filter((itm) => itm !== value);
-          return { ...o, participants };
-        });
-      } else if (name === "startingpoint") {
-        setTrip((o) => ({ ...o, startingPoint: value }));
-      } else if (name === "endingpoint") {
-        setTrip((o) => ({ ...o, endingPoint: value }));
-      } else setTrip((o) => ({ ...o, [name]: value }));
-    },
-    []
+      ({target: {value, name, checked}}) => {
+        if (name === "participants") {
+          setTrip((o) => {
+            let {participants} = o;
+            if (checked) participants.push(value);
+            else participants = participants.filter((itm) => itm !== value);
+            return {...o, participants};
+          });
+        } else if (name === "startingpoint") {
+          setTrip((o) => ({...o, startingPoint: value}));
+        } else if (name === "endingpoint") {
+          setTrip((o) => ({...o, endingPoint: value}));
+        } else setTrip((o) => ({...o, [name]: value}));
+      },
+      []
   );
 
   // handle click event of the Add button
   const handleAddClick = () => {
     console.log("handleAddClick");
     const stops = [...trip.stopPoints, ""];
-    setTrip({ ...trip, stopPoints: stops });
+    setTrip({...trip, stopPoints: stops});
   };
 
   const handleStopChange = (e, index) => {
-    const { value } = e.target;
+    const {value} = e.target;
     const stops = [...trip.stopPoints];
     stops[index] = value;
     setTrip({
@@ -144,86 +136,89 @@ const EditTripModal: React.FC<EditTripModalProps> = ({
   };
 
   return (
-    <Modal isOpen={isOpen} toggle={() => handleClose()}>
-      <ModalHeader toggle={() => handleClose()}>
-        You are ready to go... soon!
-      </ModalHeader>
-      <ModalBody>
-        <Form onSubmit={handleSubmit}>
-          <FormGroup>
-            <Label for="name" className={classNames.formLabel}>
-              Name
-            </Label>
-            <Input
-              type="text"
-              name="name"
-              id="name"
-              placeholder="Name a trip"
-              value={trip.name}
-              onChange={handleChange}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="starting-point">Starting Point</Label>
-            <Input
-              type="text"
-              name="startingpoint"
-              id="starting-point"
-              onChange={handleChange}
-              placeholder="Starting point"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="starting-point">Ending Point</Label>
-            <Input
-              type="text"
-              name="endingpoint"
-              id="ending-point"
-              onChange={handleChange}
-              placeholder="Ending point"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label>Add Stops</Label> &nbsp;
-            <Button color="success" onClick={handleAddClick}>
-              +
-            </Button>
-          </FormGroup>
-
-          {trip.stopPoints.map((stop, i) => (
-            <>
-              <FormGroup>
-                <Label for={`stopPoint${i + 1}`}>Stop Point {i}</Label>
-                <Input
+      <Modal isOpen={isOpen} toggle={() => handleClose()}>
+        <ModalHeader toggle={() => handleClose()}>
+          You are ready to go... soon!
+        </ModalHeader>
+        <ModalBody>
+          <Form onSubmit={handleSubmit}>
+            <FormGroup>
+              <Label for="name" className={classNames.formLabel}>
+                Trip Name
+              </Label>
+              <Input
                   type="text"
-                  name={`stopPoint${i}`}
+                  name="name"
+                  id="name"
+                  placeholder="Sam's evening jog to Trillium park"
+                  value={trip.name}
+                  onChange={handleChange}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="starting-point" className={classNames.formLabel}>Starting Location</Label>
+              <Input
+                  type="text"
+                  name="startingpoint"
+                  id="starting-point"
+                  onChange={handleChange}
+                  placeholder="CityPlace - Downtown"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="starting-point" className={classNames.formLabel}>Ending Location</Label>
+              <Input
+                  type="text"
+                  name="endingpoint"
                   id="ending-point"
-                  onChange={(e) => handleStopChange(e, i)}
-                  value={stop}
-                  placeholder={`Stop point ${i + 1}`}
-                />
-              </FormGroup>
-              <div className="btn-box">
-                {trip.stopPoints.length > 0 && (
-                  <Button color="danger" onClick={() => handleRemoveStop(i)}>
-                    -
-                  </Button>
-                )}
-              </div>
-            </>
-          ))}
-          <ParticipantsField handleChange={handleChange} />
-        </Form>
-      </ModalBody>
-      <ModalFooter>
-        <Button color="primary" onClick={handleSubmit}>
-          Submit
-        </Button>
-        <Button color="secondary" onClick={() => handleClose()}>
-          Cancel
-        </Button>
-      </ModalFooter>
-    </Modal>
+                  onChange={handleChange}
+                  placeholder="Trillium Park"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Button color="success" onClick={handleAddClick}>
+                Add Stops
+              </Button>
+            </FormGroup>
+
+            {trip.stopPoints.map((stop, i) => (
+                <>
+                  <FormGroup>
+                    {/*<Label for={`stopPoint${i + 1}`}>Stop Point {i}</Label>*/}
+                    <Label for={`stopPoint${i + 1}`} className={classNames.formLabel}>I'll be passing: </Label>
+                    <Media>
+                      <Media body>
+                        <Input
+                            type="text"
+                            name={`stopPoint${i}`}
+                            id="ending-point"
+                            onChange={(e) => handleStopChange(e, i)}
+                            value={stop}
+                            placeholder={`Stop point ${i + 1}`}
+                        />
+                      </Media>
+                      <Media right>
+                        {trip.stopPoints.length > 0 && (
+                            <IoIosRemoveCircleOutline className={classNames.stopRemoveIcon}
+                                                      onClick={() => handleRemoveStop(i)}/>
+                        )}
+                      </Media>
+                    </Media>
+                  </FormGroup>
+                </>
+            ))}
+            <ParticipantsField handleChange={handleChange}/>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
+          <Button color="secondary" onClick={() => handleClose()}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
   );
 };
 
