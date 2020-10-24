600cc7228c2d8509a077384705bd33f3a219bf41
Shun Cheung <shunkongcheung@gmail.com>
2020-10-24 01:38:46 -0400

    feat(types): trip and request

--- src/types.ts
+++ src/types.ts
@@ -11,3 +11,24 @@ export interface Buddy {
   meetings: Array<string>;
   buddyUid: string;
 }
+
+export interface Trip {
+  name: string;
+  // path: any; // TODO: not decided yet
+  startAt: Date;
+
+  isFinished: boolean;
+
+  participants: Array<string>;
+  createdByUid: string;
+
+  createdAt: Date;
+}
+
+export type RequestStatus = "pending" | "accepted" | "rejected";
+
+export interface Request {
+  status: RequestStatus;
+  tripUid: string;
+  createdByUid: string;
+}
