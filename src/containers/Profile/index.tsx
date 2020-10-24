383f923f6ca3b046ec22e57b7f1da04d616d4755
kavindu <narathotakyl@gmail.com>
2020-10-23 21:40:30 -0400

    qm:kavindu:front-end:profile page updated

--- src/containers/Profile/index.tsx
+++ src/containers/Profile/index.tsx
@@ -1,43 +1,43 @@
-import React, { memo } from "react";
+import React, {memo} from "react";
 import Link from "next/link";
 import firebase from "firebase";
-import {
-  Button,
-  Card,
-  CardBody,
-  CardSubtitle,
-  CardTitle,
-  CardImg,
-  Container,
-} from "reactstrap";
+import {Button, Card, CardBody, Container, FormGroup, Input,} from "reactstrap";
 
 import classNames from "./Profile.module.css";
 
-interface ProfileProps {}
+interface ProfileProps {
+}
 
 const Profile: React.FC<ProfileProps> = () => {
   const user = firebase.auth().currentUser;
 
   return (
-    <Container>
-      <Card className={classNames.card}>
-        <CardImg
-          top
-          width="100%"
-          src={user.photoURL}
-          alt="user-profile-image"
-        ></CardImg>
-        <CardBody>
-          <CardTitle>{user.displayName}</CardTitle>
-          <CardSubtitle>{user.email}</CardSubtitle>
-          <Link href="/login">
-            <Button color="danger" className={classNames.logoutBtn}>
-              LOGOUT
-            </Button>
-          </Link>
-        </CardBody>
-      </Card>
-    </Container>
+      <Container>
+        <Card className={classNames.card}>
+
+          <img className={classNames.avatar} src={user.photoURL} alt="user-profile-image"/>
+          <CardBody>
+            <FormGroup>
+              <label className={classNames.formLabel}>First Name</label>
+              <Input value={user.displayName.split(' ')[0]} alt="First Name"/>
+            </FormGroup>
+            <FormGroup>
+              <label className={classNames.formLabel}>Last Name</label>
+              <Input value={user.displayName.split(' ')[1]} alt="Last Name"/>
+            </FormGroup>
+            <FormGroup>
+              <label className={classNames.formLabel}>Email</label>
+              <Input value={user.email} alt="Email"/>
+            </FormGroup>
+
+            <Link href="/login">
+              <Button color="danger" className={classNames.logoutBtn}>
+                LOGOUT
+              </Button>
+            </Link>
+          </CardBody>
+        </Card>
+      </Container>
   );
 };
 
