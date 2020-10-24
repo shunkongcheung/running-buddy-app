c31dca0136d02613d73a8c361cfc3393ca1b5a5e
Shun Cheung <shunkongcheung@gmail.com>
2020-10-24 09:50:23 -0400

    feat(TripList): state handling at Trip

--- src/containers/Trip/index.tsx
+++ src/containers/Trip/index.tsx
@@ -1,9 +1,13 @@
-import React, { memo } from "react";
-import { Button, Container, Nav, TabContent, TabPane } from "reactstrap";
+import React, { memo, useCallback } from "react";
+import { Container, Nav, TabContent, TabPane } from "reactstrap";
 
 import EditTrip from "./EditTrip";
 import TabNavItem from "./TabNavItem";
 import classNames from "./Trip.module.css";
+import TripList from "./TripList";
+
+import { LineButton } from "../../components";
+import useTripListState from "./useTripListState";
 
 interface TripProps {}
 
@@ -12,15 +16,33 @@ type TabName = "upcoming" | "requested" | "finished";
 const Trip: React.FC<TripProps> = () => {
   const [isOpen, setIsOpen] = React.useState(false);
   const [activeTab, setActiveTab] = React.useState<TabName>("upcoming");
+  const upcoming = useTripListState(false);
+  const finished = useTripListState(true);
+
+  const handleClose = useCallback(
+    (isRefresh) => {
+      // on close, only upcoming trip can be created
+      if (isRefresh) upcoming.updateTrips();
+
+      setIsOpen(false);
+    },
+    [upcoming.updateTrips]
+  );
+
+  const handleFinished = useCallback(() => {
+    upcoming.updateTrips();
+    finished.updateTrips();
+  }, [upcoming.updateTrips, finished.updateTrips]);
+
   return (
     <Container>
-      <EditTrip handleClose={() => setIsOpen(false)} isOpen={isOpen} />
-      <div className={classNames.heading}>
+      <EditTrip handleClose={handleClose} isOpen={isOpen} />
+      <Container className={classNames.heading}>
         <h2>Manage Trips</h2>
-        <Button color="primary" onClick={() => setIsOpen(true)}>
+        <LineButton color="primary" onClick={() => setIsOpen(true)}>
           Create Trip
-        </Button>
-      </div>
+        </LineButton>
+      </Container>
       <Nav tabs>
         <TabNavItem
           activeName={activeTab}
@@ -42,11 +64,23 @@ const Trip: React.FC<TripProps> = () => {
         />
       </Nav>
       <TabContent activeTab={activeTab}>
-        <TabPane tabId="1">
-          <h4>Tab 1 Contents</h4>
+        <TabPane tabId="upcoming">
+          <TripList
+            isFinished={false}
+            loading={upcoming.loading}
+            trips={upcoming.trips}
+            handleFinished={handleFinished}
+          />
         </TabPane>
-        <TabPane tabId="2">
+        <TabPane tabId="requested">
           <h4>Tab2 contents</h4>
+        </TabPane>
+        <TabPane tabId="finished">
+          <TripList
+            isFinished={false}
+            loading={finished.loading}
+            trips={finished.trips}
+          />
         </TabPane>
       </TabContent>
     </Container>
