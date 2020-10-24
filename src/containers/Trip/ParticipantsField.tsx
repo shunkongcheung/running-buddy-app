c2474f0c857504ee366057c5756978d52f934991
Jaswinder Layal <jlayal@loyalty.com>
2020-10-24 19:28:31 -0400

    chips for buddies input

--- src/containers/Trip/ParticipantsField.tsx
+++ src/containers/Trip/ParticipantsField.tsx
@@ -1,7 +1,7 @@
 import React, { memo } from "react";
 import firebase from "firebase";
 import { FormGroup, Input, Label } from "reactstrap";
-import sphereKnn from 'sphere-knn'
+import sphereKnn from "sphere-knn";
 
 import { Buddy, RegisteredUser } from "../../types";
 
@@ -24,7 +24,7 @@ interface Coords {
 
 interface ParticipantsFieldProps {
   handleChange: ({ target: { name, value } }) => any;
-  coords: {[key:string]:{placeName:String,location:Coords} };
+  coords: { [key: string]: { placeName: String; location: Coords } };
 }
 
 const getBuddies = async (): Promise<Array<BuddyItem>> => {
@@ -39,7 +39,7 @@ const getBuddies = async (): Promise<Array<BuddyItem>> => {
 
   if (docRef.empty) return [];
   const buddies = [];
-  docRef.forEach((itm) => {
+  docRef.forEach(itm => {
     const buddy = itm.data();
     const uid = itm.id;
     buddies.push({ ...buddy, uid } as BuddyItem);
@@ -52,7 +52,7 @@ const getBuddyUsers = async (
 ): Promise<Array<BuddyUser>> => {
   const db = firebase.firestore();
   const buddyUsers = await Promise.all(
-    buddies.map(async (itm) => {
+    buddies.map(async itm => {
       const docRef = await db
         .collection("registered-users")
         .doc(itm.buddyUid)
@@ -64,7 +64,7 @@ const getBuddyUsers = async (
         uid,
         ...data,
         lastLoggedInAt,
-        buddyId: itm.uid,
+        buddyId: itm.uid
       } as BuddyUser;
     })
   );
@@ -73,7 +73,7 @@ const getBuddyUsers = async (
 
 const ParticipantsField: React.FC<ParticipantsFieldProps> = ({
   handleChange,
-  coords={}
+  coords = {}
 }) => {
   const [buddies, setBuddies] = React.useState<Array<BuddyUser>>([]);
 
@@ -86,53 +86,49 @@ const ParticipantsField: React.FC<ParticipantsFieldProps> = ({
     initializeBuddies();
   }, []);
 
-
-  React.useEffect(()=>{
-      if(Object.keys(coords).length){
-        const userCords= Object.values(coords);
-        const lookup    = sphereKnn(userCords)
-        //  const nearestCoord= lookup(someLatitude, someLongitude, 10000, 1000000)
-        console.log("buddies",buddies)
-      }
-      
-  })
-
   if (!buddies.length) return <></>;
 
   return (
     <FormGroup tag="fieldset" className={classNames.container}>
-      {buddies.map(({ uid, displayName,latitude,longitude }, idx) => {
-        let distance;
-        let placeName;
-        if(Object.keys(coords).length){
-          const userCords= Object.values(coords).map(v=>v.location);
-          const lookup    = sphereKnn(userCords)
-          const sortedCords= lookup(latitude, longitude, 10000, 1000000)
-          const nearestCoord=sortedCords[0];
-          const location=Object.values(coords).find(v=>v.location===nearestCoord)
-          placeName= location.placeName
-         distance= getDistanceBetweenCoords({latitude,longitude},{latitude:nearestCoord.lat,longitude:nearestCoord.lng})
-          
+      {buddies.map(
+        ({ uid, displayName, latitude, longitude, photoURL }, idx) => {
+          let distance;
+          let placeName;
+          if (Object.keys(coords).length) {
+            const userCords = Object.values(coords).map(v => v.location);
+            const lookup = sphereKnn(userCords);
+            const sortedCords = lookup(latitude, longitude, 10000, 1000000);
+            const nearestCoord = sortedCords[0];
+            const location = Object.values(coords).find(
+              v => v.location === nearestCoord
+            );
+            placeName = location.placeName;
+            distance = getDistanceBetweenCoords(
+              { latitude, longitude },
+              { latitude: nearestCoord.lat, longitude: nearestCoord.lng }
+            );
+          }
+          return (
+            <FormGroup check key={`ParticipantsField-${uid}-${idx}`}>
+              <Label for={uid} className={classNames.chipWrapper}>
+                <Input
+                  type="checkbox"
+                  name="participants"
+                  value={uid}
+                  id={uid}
+                  onChange={handleChange}
+                />
+                <div class="chip" className={classNames.chip}>
+                  <img src={photoURL} alt="Person" width="96" height="96" />
+                  {displayName}
+                  {distance && <b> - {Math.round(distance)} km</b>}
+                  {placeName && <span>({placeName})</span>}
+                </div>
+              </Label>
+            </FormGroup>
+          );
         }
-       return (<FormGroup check key={`ParticipantsField-${uid}-${idx}`}>
-          <Label check>
-            <Input
-              type="checkbox"
-              name="participants"
-              value={uid}
-              onChange={handleChange}
-            />
-            {displayName}
-            {
-              
-              distance && (<b> - {Math.round(distance)} km</b>)
-
-            }  {placeName && (<p>({placeName})</p>)}
-                 
-
-          </Label>
-        </FormGroup>
-      )})}
+      )}
     </FormGroup>
   );
 };
