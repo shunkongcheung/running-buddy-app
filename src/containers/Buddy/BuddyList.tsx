43ae3c6b12be26a1f737992c6d1cda14496ffd7f
kavindu <narathotakyl@gmail.com>
2020-10-24 14:58:06 -0400

    qm:kavindu:front-end:buddy list - list ui updated with the profile picture

--- src/containers/Buddy/BuddyList.tsx
+++ src/containers/Buddy/BuddyList.tsx
@@ -6,6 +6,8 @@ import {RegisteredUser} from "../../types";
 import {LineButton, PlaceHolder} from "../../components";
 import {getDistanceBetweenCoords, getUserCoord} from "../../utils";
 
+import classNames from "./BuddyList.module.css";
+
 interface BuddyUser extends RegisteredUser {
   buddyId: string;
   uid: string;
@@ -21,84 +23,85 @@ interface Coord {
   latitude: number | null;
 }
 
-const BuddyList: React.FC<BuddyListProps> = ({ buddies, updateBuddies }) => {
+const BuddyList: React.FC<BuddyListProps> = ({buddies, updateBuddies}) => {
   const [userCoord, setUserCoord] = React.useState<Coord>({
     longitude: null,
     latitude: null,
   });
 
   const unlinkBuddy = React.useCallback(
-    async (buddyId: string) => {
-      const db = firebase.firestore();
-      await db.collection("buddies").doc(buddyId).delete();
-      await updateBuddies();
-    },
-    [updateBuddies]
+      async (buddyId: string) => {
+        const db = firebase.firestore();
+        await db.collection("buddies").doc(buddyId).delete();
+        await updateBuddies();
+      },
+      [updateBuddies]
   );
   React.useEffect(() => {
     const initializeUserCoord = async () => {
-      const { coords } = await getUserCoord();
+      const {coords} = await getUserCoord();
       setUserCoord(coords);
     };
     initializeUserCoord();
   }, [updateBuddies]);
 
   return (
-    <Container>
-      <ListGroup>
-        {buddies.map(
-          (
-            {
-              displayName,
-              email,
-              buddyId,
-              longitude,
-              latitude,
-              lastLoggedInAt,
-            },
-            idx
-          ) => (
-            <ListGroupItem key={`Buddy-${displayName}-${idx}-${buddyId}`}>
-              <Media>
-                <Media left>
-                  <Media object src="/user_1.png" width={60}/>
-                </Media>
-                <Media body>
-                  <Media heading>{displayName}</Media>
-                  <h6>{email}</h6>
-                  <Media>
-                    <small>
-                      Last Logged in at: {lastLoggedInAt.toLocaleString()}
-                    </small>
-                  </Media>
-                  {userCoord.latitude !== null && userCoord.longitude !== null && (
-                    <div>
-                      <small>
-                        About{" "}
-                        {getDistanceBetweenCoords(
-                          { longitude, latitude },
-                          userCoord
-                        ).toFixed()}{" "}
-                        km from you.
-                      </small>
-                    </div>
-                  )}
-                </Media>
-                <Media right>
-                  <LineButton
-                    lineColor="#ef3648"
-                    onClick={() => unlinkBuddy(buddyId)}
-                  >
-                    Unlink
-                  </LineButton>
-                </Media>
-              </Media>
-            </ListGroupItem>
-          )
-        )}
-        {!buddies.length && <PlaceHolder>Add a buddy!</PlaceHolder>}
-      </ListGroup>
-    </Container>
+      <Container>
+        <ListGroup>
+          {buddies.map(
+              (
+                  {
+                    displayName,
+                    email,
+                    photoURL,
+                    buddyId,
+                    longitude,
+                    latitude,
+                    lastLoggedInAt,
+                  },
+                  idx
+              ) => (
+                  <ListGroupItem key={`Buddy-${displayName}-${idx}-${buddyId}`}>
+                    <Media>
+                      <Media left className={classNames.mediaLeft}>
+                        <Media className={classNames.avatar} object src={photoURL} width={60}/>
+                      </Media>
+                      <Media body>
+                        <h5>{displayName}</h5>
+                        <h6 className={classNames.emailH6}>{email}</h6>
+                        <Media>
+                          <small>
+                            Last Logged in at: {lastLoggedInAt.toLocaleString()}
+                          </small>
+                        </Media>
+                        {userCoord.latitude !== null && userCoord.longitude !== null && (
+                            <div>
+                              <small>
+                                About{" "}
+                                {getDistanceBetweenCoords(
+                                    {longitude, latitude},
+                                    userCoord
+                                ).toFixed()}{" "}
+                                km from you.
+                              </small>
+                            </div>
+                        )}
+                      </Media>
+                      <Media right>
+                        <LineButton
+                            lineColor="#ef3648"
+                            onClick={() => unlinkBuddy(buddyId)}
+                        >
+                          Unlink
+                        </LineButton>
+                      </Media>
+                    </Media>
+                  </ListGroupItem>
+              )
+          )}
+          {!buddies.length && <PlaceHolder>Add a buddy!</PlaceHolder>}
+        </ListGroup>
+      </Container>
   );
 };
 
