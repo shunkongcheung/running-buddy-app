0288550824cd5d0481a0aaf59bcb4b53af5cf3f1
Shun Cheung <shunkongcheung@gmail.com>
2020-10-23 20:24:40 -0400

    feat(profile): redirect

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,3 +1,14 @@
+import { GetServerSidePropsContext } from "next";
 import Profile from "../containers/Profile";
+
+export const getServerSideProps = (ctx: GetServerSidePropsContext) => {
+  if (!ctx.req.headers.cookie) {
+    ctx.res.setHeader("location", "/login?goTo=/profile");
+    ctx.res.statusCode = 302;
+    ctx.res.end();
+  }
+
+  return { props: {} };
+};
 
 export default () => <Profile />;
