17df18cbea6e3ac828d667baecb5b59c3c9e4fe9
Shun Cheung <shunkongcheung@gmail.com>
2020-10-24 01:14:17 -0400

    feat(Profile): handleLogout

--- src/containers/Profile/index.tsx
+++ src/containers/Profile/index.tsx
@@ -1,43 +1,67 @@
-import React, {memo} from "react";
-import Link from "next/link";
+import React, { memo } from "react";
+import { useRouter } from "next/router";
 import firebase from "firebase";
-import {Button, Card, CardBody, Container, FormGroup, Input,} from "reactstrap";
+import {
+  Button,
+  Card,
+  CardBody,
+  Container,
+  FormGroup,
+  Input,
+} from "reactstrap";
 
 import classNames from "./Profile.module.css";
 
-interface ProfileProps {
-}
+interface ProfileProps {}
 
 const Profile: React.FC<ProfileProps> = () => {
   const user = firebase.auth().currentUser;
+  const router = useRouter();
+  const handleLogout = React.useCallback(() => {
+    firebase.auth().signOut();
+    router.push("/login");
+  }, [router]);
 
   return (
-      <Container>
-        <Card className={classNames.card}>
+    <Container>
+      <Card className={classNames.card}>
+        <img
+          className={classNames.avatar}
+          src={user.photoURL}
+          alt="user-profile-image"
+        />
+        <CardBody>
+          <FormGroup>
+            <label className={classNames.formLabel}>First Name</label>
+            <Input
+              value={user.displayName.split(" ")[0]}
+              alt="First Name"
+              disabled
+            />
+          </FormGroup>
+          <FormGroup>
+            <label className={classNames.formLabel}>Last Name</label>
+            <Input
+              value={user.displayName.split(" ")[1]}
+              alt="Last Name"
+              disabled
+            />
+          </FormGroup>
+          <FormGroup>
+            <label className={classNames.formLabel}>Email</label>
+            <Input value={user.email} alt="Email" disabled />
+          </FormGroup>
 
-          <img className={classNames.avatar} src={user.photoURL} alt="user-profile-image"/>
-          <CardBody>
-            <FormGroup>
-              <label className={classNames.formLabel}>First Name</label>
-              <Input value={user.displayName.split(' ')[0]} alt="First Name" disabled/>
-            </FormGroup>
-            <FormGroup>
-              <label className={classNames.formLabel}>Last Name</label>
-              <Input value={user.displayName.split(' ')[1]} alt="Last Name" disabled/>
-            </FormGroup>
-            <FormGroup>
-              <label className={classNames.formLabel}>Email</label>
-              <Input value={user.email} alt="Email" disabled/>
-            </FormGroup>
-
-            <Link href="/login">
-              <Button color="danger" className={classNames.logoutBtn}>
-                LOGOUT
-              </Button>
-            </Link>
-          </CardBody>
-        </Card>
-      </Container>
+          <Button
+            color="danger"
+            className={classNames.logoutBtn}
+            onClick={handleLogout}
+          >
+            LOGOUT
+          </Button>
+        </CardBody>
+      </Card>
+    </Container>
   );
 };
 
