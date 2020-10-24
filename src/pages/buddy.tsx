ff6372aff7d2d267a58c2518965385712e1b33bc
Shun Cheung <shunkongcheung@gmail.com>
2020-10-24 00:39:37 -0400

    feat(pages): no redirect

--- src/pages/buddy.tsx
+++ src/pages/buddy.tsx
@@ -1,14 +1,3 @@
-import { GetServerSidePropsContext } from "next";
 import Buddy from "../containers/Buddy";
-
-export const getServerSideProps = (ctx: GetServerSidePropsContext) => {
-  if (!ctx.req.headers.cookie) {
-    ctx.res.setHeader("location", "/login?goTo=/profile");
-    ctx.res.statusCode = 302;
-    ctx.res.end();
-  }
-
-  return { props: {} };
-};
 
 export default () => <Buddy />;
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,14 +1,3 @@
-import { GetServerSidePropsContext } from "next";
 import Profile from "../containers/Profile";
-
-export const getServerSideProps = (ctx: GetServerSidePropsContext) => {
-  if (!ctx.req.headers.cookie) {
-    ctx.res.setHeader("location", "/login?goTo=/profile");
-    ctx.res.statusCode = 302;
-    ctx.res.end();
-  }
-
-  return { props: {} };
-};
 
 export default () => <Profile />;
