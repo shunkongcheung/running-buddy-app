62a3292e0a355fe809e53f63dc34ea1cd352013a
Shun Cheung <shunkongcheung@gmail.com>
2020-10-25 15:30:46 -0400

    bugfix(TripRecords): filter trip by rounds

--- src/containers/Profile/TripRecords.tsx
+++ src/containers/Profile/TripRecords.tsx
@@ -1,10 +1,12 @@
-import React, {memo} from "react";
+import React, { memo } from "react";
 import firebase from "firebase/app";
-import {ListGroup, ListGroupItem, Media} from "reactstrap";
+import { ListGroup, ListGroupItem, Media } from "reactstrap";
 
-import {Trip} from "../../types";
+import { Trip } from "../../types";
 
 import classNames from "./TripRecords.module.css";
+
+import { PlaceHolder } from "../../components";
 
 interface TripItem extends Trip {
   uid: string;
@@ -16,17 +18,21 @@ const getFinishedTrips = async () => {
   if (!userId) return [];
 
   const docRef = await firebase
-      .firestore()
-      .collection("trips")
-      .where("createdByUid", "==", userId)
-      .get();
+    .firestore()
+    .collection("trips")
+    .where("createdByUid", "==", userId)
+    .get();
   if (!docRef.size) return [];
 
   const trips: Array<TripItem> = [];
   docRef.forEach((item) => {
     const data = item.data();
+
+    // only show it if you went on to this trips before
+    if (!data.rounds.length) return;
+
     const createdAt = data.createdAt.toDate();
-    trips.push({...data, uid: item.id, createdAt} as TripItem);
+    trips.push({ ...data, uid: item.id, createdAt } as TripItem);
   });
 
   return trips;
@@ -44,26 +50,29 @@ const TripRecords: React.FC = () => {
   }, []);
 
   return (
-      <ListGroup>
-        {trips.map((trip) => (
-            <ListGroupItem key={`TripRecordItem-${trip.uid}`}>
-              <Media>
-                <Media className={classNames.mediaLeft} left>
-                  <Media className={classNames.avatar} object src="/trip-old.png"/>
-                </Media>
-                <Media body>
-                  <h5>{trip.name}</h5>
-                  <h6 className={classNames.emailH6}>
-                    You went on this trip for {trip.rounds.length} time(s)
-                  </h6>
-                  <h6 className={classNames.emailH6}>
-                    You were joined by {trip.participants.length} of your buddies
-                  </h6>
-                </Media>
-              </Media>
-            </ListGroupItem>
-        ))}
-      </ListGroup>
+    <ListGroup>
+      {trips.map((trip) => (
+        <ListGroupItem key={`TripRecordItem-${trip.uid}`}>
+          <Media>
+            <Media className={classNames.mediaLeft} left>
+              <Media className={classNames.avatar} object src="/trip-old.png" />
+            </Media>
+            <Media body>
+              <h5>{trip.name}</h5>
+              <h6 className={classNames.emailH6}>
+                You went on this trip for {trip.rounds.length} time(s)
+              </h6>
+              <h6 className={classNames.emailH6}>
+                You were joined by {trip.participants.length} of your buddies
+              </h6>
+            </Media>
+          </Media>
+        </ListGroupItem>
+      ))}
+      {trips.length === 0 && (
+        <PlaceHolder>You haven't went onto any trips</PlaceHolder>
+      )}
+    </ListGroup>
   );
 };
 
