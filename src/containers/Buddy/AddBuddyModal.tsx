b619616364e27da2018e2120906f189b8eba37e9
kavindu <narathotakyl@gmail.com>
2020-10-25 14:27:08 -0400

    qm:kavindu:front-end: AddBuddyModal.tsx modal styles

--- src/containers/Buddy/AddBuddyModal.tsx
+++ src/containers/Buddy/AddBuddyModal.tsx
@@ -1,19 +1,10 @@
-import React, { memo } from "react";
+import React, {memo} from "react";
 import firebase from "firebase/app";
-import {
-  Alert,
-  Button,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-} from "reactstrap";
+import {Alert, Button, Form, FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader,} from "reactstrap";
 
-import { Buddy, RegisteredUser } from "../../types";
+import {Buddy, RegisteredUser} from "../../types";
+
+import classNames from "./AddBuddyModal.module.css";
 
 interface AddBuddyModalProps {
   handleClose: (refresh?: boolean) => any;
@@ -28,15 +19,15 @@ const getBuddyProfile = async (email: string): Promise<BuddyProfile> => {
   // find by email
   const db = firebase.firestore();
   const docRef = await db
-    .collection("registered-users")
-    .where("email", "==", email)
-    .get();
+      .collection("registered-users")
+      .where("email", "==", email)
+      .get();
 
   if (docRef.size !== 1) throw Error("User not found");
 
   return new Promise((resolve) => {
     docRef.forEach((itm) =>
-      resolve({ ...itm.data(), uid: itm.id } as BuddyProfile)
+        resolve({...itm.data(), uid: itm.id} as BuddyProfile)
     );
   });
 };
@@ -44,10 +35,10 @@ const getBuddyProfile = async (email: string): Promise<BuddyProfile> => {
 const validateBuddyNotExist = async (ownerUid: string, buddyUid: string) => {
   const db = firebase.firestore();
   const docRef = await db
-    .collection("buddies")
-    .where("ownerUid", "==", ownerUid)
-    .where("buddyUid", "==", buddyUid)
-    .get();
+      .collection("buddies")
+      .where("ownerUid", "==", ownerUid)
+      .where("buddyUid", "==", buddyUid)
+      .get();
 
   if (!docRef.empty) throw Error("Buddy already exists!");
 };
@@ -58,66 +49,68 @@ const storeBuddy = async (buddy: Buddy) => {
 };
 
 const AddBuddyModal: React.FC<AddBuddyModalProps> = ({
-  handleClose,
-  isOpen,
-}) => {
+                                                       handleClose,
+                                                       isOpen,
+                                                     }) => {
   const [isErr, setErr] = React.useState(false);
   const [email, setEmail] = React.useState("");
 
   const handleSearch = React.useCallback(
-    async (e) => {
-      if (e) e.preventDefault();
+      async (e) => {
+        if (e) e.preventDefault();
 
-      const { uid } = firebase.auth().currentUser;
-      try {
-        const buddyProfile = await getBuddyProfile(email);
+        const {uid} = firebase.auth().currentUser;
+        try {
+          const buddyProfile = await getBuddyProfile(email);
 
-        await validateBuddyNotExist(uid, buddyProfile.uid);
+          await validateBuddyNotExist(uid, buddyProfile.uid);
 
-        await storeBuddy({
-          ownerUid: uid,
-          meetings: [],
-          buddyUid: buddyProfile.uid,
-        });
-        handleClose(true);
-      } catch (err) {
-        console.error(err);
-        setErr(true);
-      }
-    },
-    [email]
+          await storeBuddy({
+            ownerUid: uid,
+            meetings: [],
+            buddyUid: buddyProfile.uid,
+          });
+          handleClose(true);
+        } catch (err) {
+          console.error(err);
+          setErr(true);
+        }
+      },
+      [email]
   );
 
   return (
-    <Modal isOpen={isOpen} toggle={() => handleClose()}>
-      <ModalHeader toggle={() => handleClose()}>Add a Buddy!</ModalHeader>
-      <ModalBody>
-        {isErr && <Alert color="danger">Invalid Email input</Alert>}
-        <Form onSubmit={handleSearch}>
-          <FormGroup>
-            <Label for="email">Email</Label>
-            <Input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Search a user"
-              onChange={({ target: { value } }) => {
-                setEmail(value);
-                setErr(false);
-              }}
-            />
-          </FormGroup>
-        </Form>
-      </ModalBody>
-      <ModalFooter>
-        <Button color="primary" onClick={handleSearch}>
-          Add
-        </Button>
-        <Button color="secondary" onClick={() => handleClose()}>
-          Cancel
-        </Button>
-      </ModalFooter>
-    </Modal>
+      <Modal isOpen={isOpen} toggle={() => handleClose()}>
+        <ModalHeader toggle={() => handleClose()}>Add a Buddy!</ModalHeader>
+        <ModalBody className={classNames.well}>
+          <div>
+            {isErr && <Alert color="warning">Invalid Email address</Alert>}
+            <Form onSubmit={handleSearch}>
+              <FormGroup>
+                <Label for="email" className={classNames.formLabel}>Buddy's email address</Label>
+                <Input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="john@great.com"
+                    onChange={({target: {value}}) => {
+                      setEmail(value);
+                      setErr(false);
+                    }}
+                />
+              </FormGroup>
+            </Form>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={handleSearch}>
+            Add
+          </Button>
+          <Button color="secondary" onClick={() => handleClose()}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
   );
 };
 
