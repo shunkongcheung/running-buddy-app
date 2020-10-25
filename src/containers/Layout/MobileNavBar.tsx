3a06c67a65b675e9ab5c16245d2c6beedb6c761e
kavindu <narathotakyl@gmail.com>
2020-10-25 17:18:30 -0400

    qm:kavindu: mobile nav icon layout fixed 2

--- src/containers/Layout/MobileNavBar.tsx
+++ src/containers/Layout/MobileNavBar.tsx
@@ -1,47 +1,49 @@
-import React, { memo } from "react";
+import React, {memo} from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { BsFillAlarmFill } from "react-icons/bs";
+import {useRouter} from "next/router";
 
 import classNames from "./MobileNavBar.module.css";
+import {GiPathDistance, GiThreeFriends} from "react-icons/gi";
+import {CgProfile} from "react-icons/cg";
 
-interface MobileNavBarProps {}
+interface MobileNavBarProps {
+}
 
 const MobileNavBar: React.FC<MobileNavBarProps> = () => {
   const routes = [
-    { pathname: "/trip", displayName: "Trip", icon: <BsFillAlarmFill /> },
-    { pathname: "/buddy", displayName: "Buddy", icon: <BsFillAlarmFill /> },
-    { pathname: "/profile", displayName: "Profile", icon: <BsFillAlarmFill /> },
+    {pathname: "/trip", displayName: "Trip", icon: <GiPathDistance className={classNames.fontColor}/>},
+    {pathname: "/buddy", displayName: "Buddy", icon: <GiThreeFriends className={classNames.fontColor}/>},
+    {pathname: "/profile", displayName: "Profile", icon: <CgProfile className={classNames.fontColor}/>},
   ];
-  const { pathname } = useRouter();
+  const {pathname} = useRouter();
 
   const notShowing = pathname === "/login" || pathname === "/";
   if (notShowing) return <></>;
 
   return (
-    <>
-      <div className={classNames.padder} />
-      <div className={classNames.container}>
-        {routes.map((route, idx) => {
-          const active = pathname.includes(route.pathname);
-          return (
-            <Link href={route.pathname}>
-              <div
-                className={`${classNames.routeContainer} ${
-                  active ? classNames.active : ""
-                }`}
-                key={`MobileRoute-${route.pathname}-${idx}`}
-              >
-                {route.icon}
-                <span className={classNames.routeName}>
+      <>
+        <div className={classNames.padder}/>
+        <div className={classNames.container}>
+          {routes.map((route, idx) => {
+            const active = pathname.includes(route.pathname);
+            return (
+                <Link href={route.pathname}>
+                  <div
+                      className={`${classNames.routeContainer} ${
+                          active ? classNames.active : ""
+                      }`}
+                      key={`MobileRoute-${route.pathname}-${idx}`}
+                  >
+                    {route.icon}
+                    <span className={classNames.fontColor}>
                   {route.displayName}
                 </span>
-              </div>
-            </Link>
-          );
-        })}
-      </div>
-    </>
+                  </div>
+                </Link>
+            );
+          })}
+        </div>
+      </>
   );
 };
 
