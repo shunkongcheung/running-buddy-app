d82c460b459edf078c6d6837425f5704e045b795
kavindu <narathotakyl@gmail.com>
2020-10-24 21:30:29 -0400

    qm:kavindu:front-end: RequestList.tsx styles updated

--- src/containers/Trip/EditTrip.tsx
+++ src/containers/Trip/EditTrip.tsx
@@ -1,26 +1,15 @@
-import React, { memo } from "react";
+import React, {memo} from "react";
 import axios from "axios";
 import firebase from "firebase";
-import {
-  Button,
-  Form,
-  FormGroup,
-  Input,
-  Label,
-  Media,
-  Modal,
-  ModalBody,
-  ModalFooter,
-  ModalHeader,
-} from "reactstrap";
+import {Button, Form, FormGroup, Input, Label, Media, Modal, ModalBody, ModalFooter, ModalHeader,} from "reactstrap";
 
-import { InviteRequest, Trip } from "../../types";
+import {InviteRequest, Trip} from "../../types";
 import ParticipantsField from "./ParticipantsField";
 
 import classNames from "./EditTrip.module.css";
 
-import { BiMap } from "react-icons/bi";
-import { IoMdAddCircleOutline, IoIosRemoveCircleOutline } from "react-icons/io";
+import {BiMap} from "react-icons/bi";
+import {IoIosRemoveCircleOutline, IoMdAddCircleOutline} from "react-icons/io";
 
 interface EditTripModalProps {
   handleClose: (refresh?: boolean) => any;
@@ -28,9 +17,9 @@ interface EditTripModalProps {
 }
 
 const EditTripModal: React.FC<EditTripModalProps> = ({
-  handleClose,
-  isOpen,
-}) => {
+                                                       handleClose,
+                                                       isOpen,
+                                                     }) => {
   const [trip, setTrip] = React.useState<Trip>({
     name: "",
     rounds: [],
@@ -47,90 +36,90 @@ const EditTripModal: React.FC<EditTripModalProps> = ({
 
   const makeGetRequest = async (address) => {
     return axios.get(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.googleAPIKey}`
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.googleAPIKey}`
     );
   };
 
   const handleSubmit = React.useCallback(
-    async (e) => {
-      if (e) e.preventDefault();
-      const db = firebase.firestore();
+      async (e) => {
+        if (e) e.preventDefault();
+        const db = firebase.firestore();
 
-      const listAddresses = [
-        trip.startingPoint,
-        ...trip.stopPoints,
-        trip.endingPoint,
-      ];
+        const listAddresses = [
+          trip.startingPoint,
+          ...trip.stopPoints,
+          trip.endingPoint,
+        ];
 
-      const requestList = listAddresses.map((address) =>
-        makeGetRequest(address)
-      );
+        const requestList = listAddresses.map((address) =>
+            makeGetRequest(address)
+        );
 
-      const responses = await Promise.all(requestList);
-      const coords = responses.map((res) => {
-        if (
-          res.data &&
-          res.data.results[0].geometry &&
-          res.data.results[0].geometry.location
-        ) {
-          return res.data.results[0].geometry.location;
+        const responses = await Promise.all(requestList);
+        const coords = responses.map((res) => {
+          if (
+              res.data &&
+              res.data.results[0].geometry &&
+              res.data.results[0].geometry.location
+          ) {
+            return res.data.results[0].geometry.location;
+          }
+        });
+
+        try {
+          const tripRef = await db
+              .collection("trips")
+              .add({...trip, participants: [], coordinates: coords});
+          const tripUid = tripRef.id;
+
+          await Promise.all([
+            ...trip.participants.map(async (invitedUserUid: string) => {
+              const request: InviteRequest = {
+                status: "pending",
+                tripUid,
+                invitedUserUid,
+                createdAt: new Date(),
+                createdByUid: trip.createdByUid,
+              };
+              return db.collection("requests").add(request);
+            }),
+          ]);
+
+          handleClose(true);
+        } catch (err) {
+          console.error(err);
         }
-      });
-
-      try {
-        const tripRef = await db
-          .collection("trips")
-          .add({ ...trip, participants: [], coordinates: coords });
-        const tripUid = tripRef.id;
-
-        await Promise.all([
-          ...trip.participants.map(async (invitedUserUid: string) => {
-            const request: InviteRequest = {
-              status: "pending",
-              tripUid,
-              invitedUserUid,
-              createdAt: new Date(),
-              createdByUid: trip.createdByUid,
-            };
-            return db.collection("requests").add(request);
-          }),
-        ]);
-
-        handleClose(true);
-      } catch (err) {
-        console.error(err);
-      }
-    },
-    [trip]
+      },
+      [trip]
   );
 
   const handleChange = React.useCallback(
-    ({ target: { value, name, checked } }) => {
-      if (name === "participants") {
-        setTrip((o) => {
-          let { participants } = o;
-          if (checked) participants.push(value);
-          else participants = participants.filter((itm) => itm !== value);
-          return { ...o, participants };
-        });
-      } else if (name === "startingpoint") {
-        setTrip((o) => ({ ...o, startingPoint: value }));
-      } else if (name === "endingpoint") {
-        setTrip((o) => ({ ...o, endingPoint: value }));
-      } else setTrip((o) => ({ ...o, [name]: value }));
-    },
-    []
+      ({target: {value, name, checked}}) => {
+        if (name === "participants") {
+          setTrip((o) => {
+            let {participants} = o;
+            if (checked) participants.push(value);
+            else participants = participants.filter((itm) => itm !== value);
+            return {...o, participants};
+          });
+        } else if (name === "startingpoint") {
+          setTrip((o) => ({...o, startingPoint: value}));
+        } else if (name === "endingpoint") {
+          setTrip((o) => ({...o, endingPoint: value}));
+        } else setTrip((o) => ({...o, [name]: value}));
+      },
+      []
   );
 
   // handle click event of the Add button
   const handleAddClick = () => {
     console.log("handleAddClick");
     const stops = [...trip.stopPoints, ""];
-    setTrip({ ...trip, stopPoints: stops });
+    setTrip({...trip, stopPoints: stops});
   };
 
   const handleStopChange = (e, index) => {
-    const { value } = e.target;
+    const {value} = e.target;
     const stops = [...trip.stopPoints];
     stops[index] = value;
     setTrip({
@@ -152,137 +141,135 @@ const EditTripModal: React.FC<EditTripModalProps> = ({
   const handleBlur = async (e) => {
     const res = await makeGetRequest(e.target.value);
     if (
-      res &&
-      res.data &&
-      res.data.results[0].geometry &&
-      res.data.results[0].geometry.location
+        res &&
+        res.data &&
+        res.data.results[0].geometry &&
+        res.data.results[0].geometry.location
     ) {
       const latLang = res.data.results[0].geometry.location;
       setCoords({
         ...coords,
-        [e.target.name]: { placeName: e.target.value, location: latLang },
+        [e.target.name]: {placeName: e.target.value, location: latLang},
       });
     }
   };
 
   return (
-    <Modal isOpen={isOpen} toggle={() => handleClose()}>
-      <ModalHeader toggle={() => handleClose()}>Plan my trip!</ModalHeader>
-      <ModalBody>
-        <Form onSubmit={handleSubmit}>
-          <FormGroup>
-            <Label for="name" className={classNames.formLabel}>
-              My trip name
-            </Label>
-            <Input
-              type="text"
-              name="startingpoint"
-              id="starting-point"
-              onChange={handleChange}
-            />
-          </FormGroup>
-
-          <hr />
-
-          <FormGroup>
-            <Label for="starting-point" className={classNames.formLabel}>
-              I'll start my run here
-            </Label>
-            <Media>
-              <Media left>
-                <BiMap className={classNames.startEndPins} />
-              </Media>
-              <Media body>
-                <Input
+      <Modal isOpen={isOpen} toggle={() => handleClose()}>
+        <ModalHeader toggle={() => handleClose()}>Plan my trip!</ModalHeader>
+        <ModalBody>
+          <Form onSubmit={handleSubmit}>
+            <FormGroup>
+              <Label for="name" className={classNames.formLabel}>
+                My trip name
+              </Label>
+              <Input
                   type="text"
                   name="startingpoint"
                   id="starting-point"
                   onChange={handleChange}
-                  placeholder="CityPlace - Downtown"
-                />
-              </Media>
-            </Media>
-          </FormGroup>
+              />
+            </FormGroup>
 
-          <div className={classNames.stopsWell}>
-            <div>
-              {trip.stopPoints.map((stop, i) => (
-                <>
-                  <FormGroup>
-                    {/*<Label for={`stopPoint${i + 1}`}>Stop Point {i}</Label>*/}
-                    <Label
-                      for={`stopPoint${i + 1}`}
-                      className={classNames.formLabel}
-                    >
-                      I'll be passing{" "}
-                    </Label>
-                    <Media>
-                      <Media body>
-                        <Input
-                          type="text"
-                          name={`stopPoint${i}`}
-                          id="ending-point"
-                          onChange={(e) => handleStopChange(e, i)}
-                          value={stop}
-                          placeholder={`Destination ${i + 1}`}
-                        />
-                      </Media>
-                      <Media right>
-                        {trip.stopPoints.length > 0 && (
-                          <IoIosRemoveCircleOutline
-                            className={classNames.stopRemoveIcon}
-                            onClick={() => handleRemoveStop(i)}
-                          />
-                        )}
-                      </Media>
-                    </Media>
-                  </FormGroup>
-                </>
-              ))}
+            <hr/>
+
+            <FormGroup>
+              <Label for="starting-point" className={classNames.formLabel}>
+                I'll start my run here
+              </Label>
+              <Media>
+                <Media left>
+                  <BiMap className={classNames.startEndPins}/>
+                </Media>
+                <Media body>
+                  <Input
+                      type="text"
+                      name="startingpoint"
+                      id="starting-point"
+                      onChange={handleChange}
+                      placeholder="CityPlace - Downtown"
+                  />
+                </Media>
+              </Media>
+            </FormGroup>
+
+            <div className={classNames.stopsWell}>
+              <div>
+                {trip.stopPoints.map((stop, i) => (
+                    <>
+                      <FormGroup>
+                        {/*<Label for={`stopPoint${i + 1}`}>Stop Point {i}</Label>*/}
+                        <Label
+                            for={`stopPoint${i + 1}`}
+                            className={classNames.formLabel}
+                        >
+                          I'll be passing{" "}
+                        </Label>
+                        <Media>
+                          <Media body>
+                            <Input
+                                type="text"
+                                name={`stopPoint${i}`}
+                                id="ending-point"
+                                onChange={(e) => handleStopChange(e, i)}
+                                value={stop}
+                                placeholder={`Destination ${i + 1}`}
+                            />
+                          </Media>
+                          <Media right>
+                            {trip.stopPoints.length > 0 && (
+                                <IoIosRemoveCircleOutline
+                                    className={classNames.stopRemoveIcon}
+                                    onClick={() => handleRemoveStop(i)}
+                                />
+                            )}
+                          </Media>
+                        </Media>
+                      </FormGroup>
+                    </>
+                ))}
+              </div>
+
+              <FormGroup onClick={handleAddClick}>
+                <IoMdAddCircleOutline className={classNames.stopAddIcon}/>
+                <span className={classNames.stopAddSpan}>Add destinations</span>
+              </FormGroup>
             </div>
 
-            <FormGroup onClick={handleAddClick}>
-              <IoMdAddCircleOutline className={classNames.stopAddIcon} />
-              <span className={classNames.stopAddSpan}>Add destinations</span>
-            </FormGroup>
-          </div>
-
-          <div className={classNames.stopsWell}>
             <FormGroup>
               <Label for="starting-point" className={classNames.formLabel}>
                 I'll finish my run here
               </Label>
               <Media>
                 <Media left>
-                  <BiMap className={classNames.startEndPins} />
+                  <BiMap className={classNames.startEndPins}/>
                 </Media>
                 <Media body>
                   <Input
-                    type="text"
-                    name="endingpoint"
-                    id="ending-point"
-                    onChange={handleChange}
-                    placeholder="Trillium Park"
+                      type="text"
+                      name="endingpoint"
+                      id="ending-point"
+                      onChange={handleChange}
+                      placeholder="Trillium Park"
                   />
                 </Media>
               </Media>
             </FormGroup>
-          </div>
 
-          <hr />
+            <hr/>
 
-          <ParticipantsField coords={coords} handleChange={handleChange} />
-        </Form>
-      </ModalBody>
-      <ModalFooter>
-        <Button color="primary" onClick={handleSubmit}>
-          Submit
-        </Button>
-        <Button color="secondary" onClick={() => handleClose()}>
-          Cancel
-        </Button>
-      </ModalFooter>
-    </Modal>
+            <ParticipantsField coords={coords} handleChange={handleChange}/>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
+          <Button color="secondary" onClick={() => handleClose()}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
   );
 };
 
--- src/containers/Trip/RequestList.tsx
+++ src/containers/Trip/RequestList.tsx
@@ -1,18 +1,18 @@
-import React, { memo } from "react";
-import { ListGroup, ListGroupItem, Media } from "reactstrap";
-import { GoRequestChanges } from "react-icons/go";
-import { TiDelete, TiTick } from "react-icons/ti";
+import React, {memo} from "react";
+import {ListGroup, ListGroupItem, Media} from "reactstrap";
+import {TiDelete, TiTick} from "react-icons/ti";
 
-import { PlaceHolder, Progress } from "../../components";
+import {PlaceHolder, Progress} from "../../components";
 
 import classNames from "./RequestList.module.css";
 import useRequestListState from "./useRequestListState";
+import Link from "next/link";
 
 interface RequestListProps {
   handleRequestUpdate: () => any;
 }
 
-const RequestList: React.FC<RequestListProps> = ({ handleRequestUpdate }) => {
+const RequestList: React.FC<RequestListProps> = ({handleRequestUpdate}) => {
   const {
     handleAccept,
     handleDecline,
@@ -20,48 +20,58 @@ const RequestList: React.FC<RequestListProps> = ({ handleRequestUpdate }) => {
     inviteRequests,
   } = useRequestListState(handleRequestUpdate);
   return (
-    <div className={classNames.container}>
-      <Progress loading={loading} />
-      <ListGroup>
-        {inviteRequests.map(
-          ({
-            uid,
-            trip: { name, participants, createdAt, uid: tripUid },
-            createdBy: { displayName },
-          }) => (
-            <ListGroupItem key={`TripListItem-${uid}`}>
-              <Media>
-                <Media left>
-                  <GoRequestChanges className={classNames.runningIcon} />
-                </Media>
-                <Media body>
-                  <h3>{name}</h3>
-                  <p>
-                    {participants.length} confirmed the invitation.
-                    <br />
-                    Trip initiated by {displayName}.
-                  </p>
-                  <small>Created at {createdAt.toLocaleString()}</small>
-                </Media>
-                <Media right>
-                  <TiTick
-                    className={classNames.acceptIcon}
-                    onClick={() => handleAccept(uid, tripUid)}
-                  />
-                  <TiDelete
-                    className={classNames.declineIcon}
-                    onClick={() => handleDecline(uid)}
-                  />
-                </Media>
-              </Media>
-            </ListGroupItem>
-          )
+      <div className={classNames.container}>
+        <Progress loading={loading}/>
+        <ListGroup>
+          {inviteRequests.map(
+              ({
+                 uid,
+                 trip: {name, participants, createdAt, uid: tripUid},
+                 createdBy: {displayName},
+               }) => (
+                  <ListGroupItem key={`TripListItem-${uid}`}>
+
+
+                    <Media>
+                      <Media className={classNames.mediaLeft} left>
+                        <Link href={`/trip/${uid}`} key={`TripListItem-${uid}`}>
+                          <Media className={classNames.avatar} object src="/trip-request.png"/>
+                        </Link>
+                      </Media>
+
+                      <Media body>
+                        <h5>{name}</h5>
+                        <h6 className={classNames.emailH6}>
+                          {participants.length} confirmed the invitation.
+                        </h6>
+                        <h6 className={classNames.emailH6}>
+                          Trip initiated by {displayName}.
+                        </h6>
+                        <h6 className={classNames.emailH6}>
+                          Created at {createdAt.toLocaleString()}
+                        </h6>
+                      </Media>
+
+                      <Media right>
+                        <TiTick
+                            className={classNames.acceptIcon}
+                            onClick={() => handleAccept(uid, tripUid)}
+                        />
+                        <TiDelete
+                            className={classNames.declineIcon}
+                            onClick={() => handleDecline(uid)}
+                        />
+                      </Media>
+                    </Media>
+
+                  </ListGroupItem>
+              )
+          )}
+        </ListGroup>
+        {!inviteRequests.length && (
+            <PlaceHolder>No pending requests.</PlaceHolder>
         )}
-      </ListGroup>
-      {!inviteRequests.length && (
-        <PlaceHolder>No pending requests.</PlaceHolder>
-      )}
-    </div>
+      </div>
   );
 };
 
